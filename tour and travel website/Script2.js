e5febd96d7cab33069d656bee76c86203e369e92
hello-aaditya <aadityakumar.prof@gmail.com>
2024-11-05 01:22:54 +0530

    Web D

--- tour and travel website/Script2.js
+++ tour and travel website/Script2.js
@@ -9,14 +9,18 @@ import {
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCh_LKbmd4OD650PN9OkO5Fkql4EiIuzY4",
-  authDomain: "signup-and-signin-d25c2.firebaseapp.com",
-  projectId: "signup-and-signin-d25c2",
-  storageBucket: "signup-and-signin-d25c2.appspot.com",
-  messagingSenderId: "156350853031",
-  appId: "1:156350853031:web:b510f731009dd8e5a3d082",
+  apiKey: "AIzaSyAyKqcaklHPW3QZWPU6jznq_DL1FeoMzBw",
+  authDomain: "travellwebapplication.firebaseapp.com",
+  projectId: "travellwebapplication",
+  storageBucket: "travellwebapplication.firebasestorage.app",
+  messagingSenderId: "279782437248",
+  appId: "1:279782437248:web:91edde2cc424b760ea1165",
+  measurementId: "G-9QC5JETF21"
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
